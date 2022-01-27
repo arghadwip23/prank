aeb849a99547dace339b3340b7d0d8fcb9914917
arghadwip23 <arghadwip23@gmail.com>
2022-01-27 18:16:19 +0530

    ifi

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 console.log('Hello World!');
-var vid = document.getElementById("vid");
-document.addEventListener("click", ()=>{
-  vid.muted = vid.unmuted;
-})
+var audio = document.getElementById("aud");
+var pageS = document.getElementById("page2");
+var pageF = document.getElementById("page1");
+var options = {
+  strings: ['Hey, there is a surprise for you my friend, just touch the screen'],
+  typeSpeed: 70,
+  showCursor:false
+};
+
+var typed = new Typed('.element', options);
+
+pageF.addEventListener("click", ()=>{
+   pageF.style.display = "none";
+   pageS.style.display = "flex";
+   audio.play();
+   console.log(audio.volume);
+});
